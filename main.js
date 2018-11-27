37a586fea0367137b2e05df50f001c663eed5828
Cam Peters <campeters215@outlook.com>
2018-11-27 14:44:12 -0600

    Edited rides perhour graph

--- main.js
+++ main.js
@@ -4,9 +4,58 @@ $(updateView)
 
 function updateView() {
    $.getJSON(BASE_URL + "/rides/count", updateRideCount )
+   $.getJSON(BASE_URL + "/rides/count/per_hour", printRideCount)
+   displayHourlyChart()
 }
 
 function updateRideCount(data) {
     numberOfRides = data.count
     $("h2#rideCount").html(numberOfRides)
 }
+
+function printRideCount(data) {
+    console.log(data)
+}
+
+function displayHourlyChart(data) {
+    var ctx = document.getElementById("ridesPerHourChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+    
+}
+    
+    
